873edb0a2f0a2be0ec18174ffa5c7957511ab611
saikrishna62892 <saikrishna62892@gmail.com>
2019-10-24 12:44:40 +0530

    CSS Fixed to signup,post_problem,view_problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      redirect_to user_profile_url(current_user)
+      redirect_to wall_path
 
 
     else
