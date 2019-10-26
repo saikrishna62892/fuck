c86b266522017964b39957fce9de0af707e72cb1
saikrishna62892 <saikrishna62892@gmail.com>
2019-10-26 20:48:58 +0530

    gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  host = 'https://workcollabwc.herokuapp.com'
+  host = 'https://obscure-castle-71852.herokuapp.com'
   config.action_mailer.default_url_options = { host:host }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
